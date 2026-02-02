acdaccb671ed32d411ad89864fb23321359c1f81
Adriano Teles <adriano.telesc@gmail.com>
2026-02-02 14:14:03 -0300

    Add mapKeys function into the map extension

--- lib/src/common/extension/map_extension.dart
+++ lib/src/common/extension/map_extension.dart
@@ -8,6 +8,9 @@ extension MapExtension<K1, V1> on Map<K1, V1> {
 
   bool anyValue(bool Function(V1) test) => values.any((value) => test(value));
 
-  Map<K1, V2> mapValues<V2>(V2 Function(V1) convertValue) =>
-      map((key, value) => MapEntry(key, convertValue(value)));
+  Map<K1, V2> mapValues<V2>(V2 Function(V1) convert) =>
+      map((key, value) => MapEntry(key, convert(value)));
+
+  Map<K2, V1> mapKeys<K2>(K2 Function(K1) convert) =>
+      map((key, value) => MapEntry(convert(key), value));
 }
